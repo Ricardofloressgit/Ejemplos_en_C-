15bb97eb29d5c9310021a2e502830692884576c3
Ricardofloressgit <155786348+Ricardofloressgit@users.noreply.github.com>
2024-01-11 21:49:19 -0400

    ejemplos_de_valores.cpp

--- ejemplos_en_C++/ejemplos_de_valores.cpp
+++ ejemplos_en_C++/ejemplos_de_valores.cpp
@@ -1,6 +1,6 @@
 /*
 Titulo :Primer ejemplos de valores tipo entero con short int 
-Autor : Juan David Meza Gonzalez/ALUM: Ricardo flores
+Autor : Ricardo flores
 Descripcion: PRIMER PROGRAMA PARA ANALIZAR SU ESTRUCTURA.
 Fecha: 2024-01-11
 
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 	double numero_decimal_g;
 	//asignando valores limite de los tipos de datos 
 	/*numero_entero_p= 32767;//<--valor limite de datos
-	cout<< "Numero entero pequeño : "<<numero_entero_p<<endl;
+	cout<< "Numero entero pequeÃ±o : "<<numero_entero_p<<endl;
 	numero_entero_p1 = 32768;//valor fuera de rango
-	cout<< "Numero entero pequeño positivo: "<<numero_entero_p1<<endl;
+	cout<< "Numero entero pequeÃ±o positivo: "<<numero_entero_p1<<endl;
 	*/
 	numero_entero = -2147483649;//<-- valor limite de datos de 0 a 4294967295
 	cout<<" Numero entero con signo: "<<numero_entero<<endl;
